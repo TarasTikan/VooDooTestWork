818af2fac9a8c9de4836e444c1a6362913470900
TarasTikan <109157753+TarasTikan@users.noreply.github.com>
2023-08-03 01:22:23 +0000

    Deploying to gh-pages from @ TarasTikan/VooDooTestWork@7b08bb5c740be4a08456ddf8b7d1ae28ea1c24fc 🚀

---
+++ index.7fa5a905.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#blackAlrert"),t=document.querySelector("#importantText"),c=document.querySelector("#imageArrow");e.addEventListener("click",(function(e){t.classList.toggle("hidden"),c.classList.toggle("rotate-180")}));
+//# sourceMappingURL=index.7fa5a905.js.map
---
+++ index.f42cb6f3.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#blackAlrert"),t=document.querySelector("#importantText"),r=document.querySelector("#imageArrow");e.addEventListener("click",(function(e){t.classList.toggle("hidden"),r.classList.toggle("rotate-180")}))}();
+//# sourceMappingURL=index.f42cb6f3.js.map
